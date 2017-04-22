84b43ef88e58b71b3fdddd2ecd80a066fe2e1363
Ciel <imwithye@gmail.com>
2017-04-22 17:12:36 +0000

    use ubuntu as base image

--- frp/run.sh
+++ frp/run.sh
@@ -5,13 +5,13 @@ pd=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
 : ${PASSWORD:=$pd}
 echo "--> password: $PASSWORD"
 echo "--> generating config file"
-cat <<EOF > /var/frp/frps.ini
+cat <<EOF > $FRP/frps.ini
 [common]
 bind_port = 7000
 dashboard_port = 7500
 privilege_mode = true
 privilege_token = ${PASSWORD}
 EOF
-echo "--> cmd: ./frps -c ./frps.ini"
+echo "--> cmd: $FRP/frps -c $FRP/frps.ini"
 echo
-./frps -c ./frps.ini
+$FRP/frps -c $FRP/frps.ini
