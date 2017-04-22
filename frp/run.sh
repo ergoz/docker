2ae06c1b5898584d5b9d5c5cab7a4af76309c793
Ciel <imwithye@gmail.com>
2017-04-22 17:31:54 +0000

    allow user change run.sh

--- frp/run.sh
+++ frp/run.sh
@@ -2,15 +2,19 @@
 
 echo "--> generating password"
 pd=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
-: ${PASSWORD:=$pd}
-echo "--> password: $PASSWORD"
+: ${TOKEN:=$pd}
+echo "--> password: $TOKEN"
 echo "--> generating config file"
+: ${DASHBOARD_USER:="admin"}
+: ${DASHBOARD_PASSWORD:="admin"}
 cat <<EOF > $FRP/frps.ini
 [common]
 bind_port = 7000
 dashboard_port = 7500
 privilege_mode = true
-privilege_token = ${PASSWORD}
+privilege_token = ${TOKEN}
+dashboard_user = ${DASHBOARD_USER}
+dashboard_pwd = ${DASHBOARD_PASSWORD}
 EOF
 echo "--> cmd: $FRP/frps -c $FRP/frps.ini"
 echo
